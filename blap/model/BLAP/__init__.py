40662e4102a05051a3f382b32040d7ad36543d4f
Constantin Pinkl <cpinkl@ethz.ch>
2024-10-14 19:27:58 +0200

    Deleting Unused Files

--- blap/model/BLAP/__init__.py
+++
@@ -1,8 +0,0 @@
-from transformers import BertTokenizer
-
-def init_tokenizer():
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokenizer.add_special_tokens({'bos_token':'[DEC]'})
-    tokenizer.add_special_tokens({'additional_special_tokens':['[ENC]']})       
-    tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]  
-    return tokenizer
